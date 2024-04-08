fd736d48620b1b1c065332cc9a1f4c46ce3b2c95
LEE HYUN <91305688+dlgus2383@users.noreply.github.com>
2024-04-08 16:06:32 +0900

    feat: 충격파 10도씩 틀어지도록 수정 그냥 나가는게 아닌 공격키를 눌러야 발동

--- Assets/Scripts/Hud/Item/Weapon/WoundsOfWind/WoundsOfWind.cs
+++ Assets/Scripts/Hud/Item/Weapon/WoundsOfWind/WoundsOfWind.cs
@@ -8,7 +8,7 @@ public class WoundsOfWind : Weapon
     {
 
         timer += Time.deltaTime;
-        if (timer > shootThreshold)
+        if (timer > shootThreshold && Input.GetKeyDown(KeyCode.Z))
         {
             timer = 0;
             Fire();
@@ -22,14 +22,17 @@ public class WoundsOfWind : Weapon
     }
     public void Fire()
     {
+        float angleOffset = Random.Range(-10f, 10f);
         Player player = Locator.player;
         Vector2 targetDirection = (player.transform.localScale.x == -1) ? Vector2.left : Vector2.right;
+        targetDirection = Quaternion.Euler(0, 0, angleOffset) * targetDirection;
         Transform bullet = TempGameManager.instance.poolManager.Get(id).transform;
         Damager damager = bullet.GetComponent<Damager>();
         damager.disable();
         damager.enable();
         bullet.position = transform.position;
-        Quaternion targetRotation = Quaternion.FromToRotation(Vector3.up, targetDirection);
+        Quaternion targetRotation = Quaternion.FromToRotation(Vector3.up, targetDirection) * Quaternion.Euler(0, 0, angleOffset);
+        // Quaternion targetRotation = Quaternion.FromToRotation(Vector3.up, targetDirection);
         bullet.rotation = targetRotation;
         bullet.GetComponent<RangedItem>().Init(damage, targetDirection , size);
         bullet.GetComponent<Damager>().damage = (int)damage;
