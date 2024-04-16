d96f0fedd6bb0630f30813bad135caf8f41ca4c2
kookmin1916 <kookmin1916@kookmin.ac.kr>
2024-04-16 15:35:15 +0900

    Rename: Sprites

---
+++ Assets/Scripts/Items/ActiveItems/TestActiveItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Progress;
+
+
+public class TestActiveItem : ActiveItem
+{
+    public override void use()
+    {
+        Debug.Log("Test Item Use");
+    }
+}
