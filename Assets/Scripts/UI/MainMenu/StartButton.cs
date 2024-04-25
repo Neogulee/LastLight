bc8c280411945b16906733ecff1b2150cef975d4
LEE HYUN <91305688+dlgus2383@users.noreply.github.com>
2024-04-25 13:14:09 +0900

    feat: 리더보드 UI 추가

--- Assets/Scripts/UI/MainMenu/StartButton.cs
+++ Assets/Scripts/UI/MainMenu/StartButton.cs
@@ -5,25 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
-    public Canvas canvas;
     public void SceneChange()
     {
-        StartCoroutine(zoom_in());
         SceneManager.LoadScene("Map_01", LoadSceneMode.Single);
         // 로딩 시간 체크 후에 Fade In , Fade Out을 넣을지 고려  
 
-    }
-
-    public IEnumerator zoom_in()
-    {
-        float duration = 1.0f;
-        float current_time = 0.0f;
-        while (current_time < duration)
-        {
-            float current_scale = Mathf.Lerp(1.0f, 1.5f, current_time / duration);
-            canvas.scaleFactor = current_scale;
-            current_time += Time.fixedDeltaTime;
-            yield return new WaitForSeconds(0.01f);
-        }
     }
 }
