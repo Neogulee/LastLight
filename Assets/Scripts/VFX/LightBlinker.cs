5235d792c5efd16f701ebb70ec75eb12fb8c8645
kookmin1916 <kookmin1916@kookmin.ac.kr>
2024-05-11 17:00:40 +0900

    Feat: 시야밖 빛 비활성화 추가

--- Assets/Scripts/VFX/LightBlinker.cs
+++ Assets/Scripts/VFX/LightBlinker.cs
@@ -9,20 +9,35 @@ public class LightBlinker : MonoBehaviour
     public float speed = 12.0f;
 
     private new Light2D light = null;
-    private float radius;
+    private float radius = 0.0f;
     private float time = 0.0f;
-    private Material material;
+    private Material material = null;
+    private new SpriteRenderer renderer = null;
     void Awake()
     {
         light = GetComponent<Light2D>();
         radius = light.pointLightOuterRadius;
-        material = GetComponent<SpriteRenderer>().material;
+        renderer = GetComponent<SpriteRenderer>();
+        material = renderer.material;
     }
 
     void FixedUpdate()
     {
+        if (!light.enabled)
+            return;
+
         time += Time.deltaTime;
         light.pointLightOuterRadius = radius * ((Mathf.Sin(speed * time) + 1.0f) / 6.0f + 1.0f - 1.5f / 6.0f);
         material.SetFloat("_CurrentTime", time);
+    }
+
+    void OnBecameInvisible()
+    {
+        light.enabled = false;
+    }
+
+    void OnBecameVisible()
+    {
+        light.enabled = true;
     }
 }
