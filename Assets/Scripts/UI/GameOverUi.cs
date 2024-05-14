4224ae545c1e21e9fa096a5eacf4fec05bec93e2
LEE HYUN <91305688+dlgus2383@users.noreply.github.com>
2024-05-14 15:37:03 +0900

    feat: 게임 종료 씬 추가
    (임시로 L 누르면 작동)

---
+++ Assets/Scripts/UI/GameOverUi.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+public class GameOverUi : MonoBehaviour
+{
+    public UnityEngine.UI.Image image1;
+    public UnityEngine.UI.Image image2;
+    public TextMeshProUGUI textMeshPro;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            StartCoroutine(ChangeAlphaOverTime());
+        }
+    }
+
+    IEnumerator ChangeAlphaOverTime()
+    {
+        
+        float elapsedTime = 0f;
+        while (elapsedTime < 2f)
+        {
+            Color image1Color = image1.color;
+            image1Color.a = Mathf.Lerp(0f, 0.6f, elapsedTime / 2f);
+            image1.color = image1Color;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        
+        elapsedTime = 0f;
+        while (elapsedTime < 2f)
+        {
+            Color image2Color = image2.color;
+            image2Color.a = Mathf.Lerp(0f, 0.8f, elapsedTime / 2f);
+            image2.color = image2Color;
+
+            Color textMeshProColor = textMeshPro.color;
+            textMeshProColor.a = Mathf.Lerp(0f, 1f, elapsedTime / 2f);
+            textMeshPro.color = textMeshProColor;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        StartCoroutine(FadeManager.Instance.LoadDiffScene("Main"));
+    }
+}
