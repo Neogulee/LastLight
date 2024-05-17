96988fb91478245e794708ce9439c52a5615aaaa
LEE HYUN <91305688+dlgus2383@users.noreply.github.com>
2024-05-17 16:30:14 +0900

    feat: 보스 사망시 GameClear 출력

---
+++ Assets/Scripts/Actors/Enemies/EnemyBoss.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBoss : Enemy
+{ 
+    
+    protected override void on_destroyed(){
+        Locator.event_manager.notify(new GameClearEvent());
+    }
+}
--- Assets/Scripts/Systems/ResultManager.cs
+++ Assets/Scripts/Systems/ResultManager.cs
@@ -4,23 +4,7 @@ using UnityEngine;
 
 public class ResultManager : MonoBehaviour
 {
-    private GameClear gameClear;
-    private GameOverUi gameOver;  
-
-    void Start()
-    {
-        gameClear = FindObjectOfType<GameClear>();
-        gameOver = FindObjectOfType<GameOverUi>();
-    }
-
-    public void GameClear()
-    {
-        gameClear.GameClearStart();
-    }
-    public void GameOver()
-    {
-        gameOver.GameOverStart();
-    }
+    
 
 
 }
