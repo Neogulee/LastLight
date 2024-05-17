4e5379cfdae5f06bcc011e0a96f8859df0050c49
DIPPER_GRAM\dippe <dipper1002@naver.com>
2024-05-17 16:31:46 +0900

    feat : 체력 흡수 초안 개발

---
+++ Assets/Scripts/Actors/Players/PlayerDamager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Damager))]
+public class PlayerDamager : MonoBehaviour
+{
+    public void on_hit()
+    {
+
+    }
+}
